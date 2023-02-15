0a828966cadeb92df491dd7071f094c7a7ba10d7
Huy Nguyen <huynp.dev@gmail.com>
2023-02-15 09:27:23 +0700

    first commit

---
+++ src/Category.php
@@ -0,0 +1,31 @@
+<?php
+namespace Huynp\Kiotviet;
+use GuzzleHttp\Client as Request;
+use Huynp\Kiotviet\Client;
+
+class Category{
+    private Client $client;
+    private array $headers;
+
+    function __construct(Client $client) {
+        $this->client = $client;
+        $this->headers = [
+            'Retailer' => $client->getRetailerCode(),
+            'Authorization' => "Bearer ".$client->getAccessToken(),
+        ];
+    }
+    public function getList($limit = 20, $page = 0){
+        $url = "https://public.kiotapi.com/categories?&currentItem={$page}&pageSize={$limit}";
+        $client = new Request([
+            'base_uri' => 'http://httpbin.org',
+            'timeout'  => 20.0,
+        ]);
+        
+        $response = $client->request('GET', $url, [
+            'headers'=>$this->headers,
+           
+        ]);
+        $body = (string)$response->getBody();
+        return json_decode($body, true)['data'];
+    }
+}
---
+++ src/Client.php
@@ -0,0 +1,45 @@
+<?php
+namespace Huynp\Kiotviet;
+use GuzzleHttp\Client as Request;
+
+class Client{
+    private string $client_id, $client_secret, $token, $retailerCode;
+
+    function __construct(string $client_id, string $client_secret) {
+        $this->client_id = $client_id;
+        $this->client_secret = $client_secret;
+        $this->token = $this->getToken();
+    }
+    public function getToken(){
+        $url = "https://id.kiotviet.vn/connect/token";
+        $client = new Request([
+            'base_uri' => 'http://httpbin.org',
+            'timeout'  => 20.0,
+        ]);
+        
+        $response = $client->request('POST', $url, [
+            'headers'=>[
+                'Content-Type' => 'application/x-www-form-urlencoded'
+            ],
+            'form_params' => [
+                'scopes'=>'PublicApi.Access',
+                'grant_type' => 'client_credentials',
+                'client_id'=>$this->client_id,
+                'client_secret'=>$this->client_secret
+                ]
+        ]);
+        $body = (string)$response->getBody();
+        $result = json_decode($body, true);
+        $accessToken = $result['access_token'];
+        $payload = json_decode(base64_decode(explode('.', $accessToken)[1]), true);
+        $this->retailerCode = $payload['client_RetailerCode'];
+        return $accessToken;
+        var_dump($body);
+    }
+    public function getRetailerCode(){
+        return $this->retailerCode;
+    }
+    public function getAccessToken(){
+        return $this->token;
+    }
+}
